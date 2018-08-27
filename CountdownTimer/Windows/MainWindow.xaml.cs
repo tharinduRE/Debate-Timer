929866d603d2140abca1f4e15b0a4204efd6e57b
Randika-User <Randika-User@DELL-INSPIRON15>
2018-08-27 13:41:50 +0530

    Time Changed

--- CountdownTimer/Windows/MainWindow.xaml.cs
+++ CountdownTimer/Windows/MainWindow.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Shell;
 
 using Hourglass.Extensions;
